2445542255588736d89fa77660171a4376740a90
sadik ali <sadik.ali@5nance.com>
2026-02-11 14:31:44 +0530

    backup

---
+++ routes-bkp/auth.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const { authenticate, authorize } = require("../middleware/authMiddleware");
+const {
+  registerEmployee,
+  login,
+  uploadAvatar,
+  getRoles,
+  getProfile,
+  updateMobile,
+  forgotPasswordRequest,
+  resetPasswordSelf,
+  getCurrentUser,
+  checkEmailExists,
+  admins,
+} = require("../controllers/authController");
+
+const router = express.Router();
+
+// Multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const userId = req.user.id;
+    if (!userId) return cb(new Error("UserId missing"), null);
+    const ext = path.extname(file.originalname) || ".jpg";
+    cb(null, `${userId}${ext}`);
+  },
+});
+const upload = multer({ storage });
+
+router.get("/me", authenticate, getCurrentUser);
+
+// public
+router.post("/login", login);
+
+// protected
+router.get(
+  "/admins",
+  authenticate,
+  authorize(["ceo", "hr", "superadmin"]),
+  admins,
+);
+router.post(
+  "/checkemail",
+  authenticate,
+  authorize(["superadmin", "admin"]),
+  checkEmailExists
+);
+router.post(
+  "/register",
+  authenticate,
+  authorize(["ceo", "hr"]),
+  registerEmployee,
+);
+router.post("/mobileUpdate", authenticate, updateMobile);
+
+router.get("/roles", authenticate, getRoles);
+router.get("/profile", authenticate, getProfile);
+
+router.post("/upload", authenticate, upload.single("avatar"), uploadAvatar);
+// forgot password
+router.post("/forgot_password", forgotPasswordRequest);
+router.post("/reset_password_self", resetPasswordSelf);
+
+module.exports = router;
---
+++ routes-bkp/hrms.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllEmployee,
+  getAllLeaveType,
+  applyLeave,
+  getMyLeaves,
+  updateLeaves,
+  getOtherLeaveRequest,
+  getPendingLeavesForHr,
+} = require("../controllers/hrmsController");
+const { authenticate, authorize } = require("../middleware/authMiddleware");
+
+router.get("/all-employee", authenticate, getAllEmployee);
+router.get("/leave-types", authenticate, getAllLeaveType);
+router.get("/my-leaves", authenticate, getMyLeaves);
+router.post("/leave-apply", authenticate, applyLeave);
+router.get(
+  "/other-leaves-request",
+  authenticate,
+  authorize(["superadmin", "hr"]),
+  getOtherLeaveRequest
+);
+router.put(
+  "/update-leave-status",
+  authorize(["superadmin", "hr"]),
+  authenticate,
+  updateLeaves
+);
+
+module.exports = router;
